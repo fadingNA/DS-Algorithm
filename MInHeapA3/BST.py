4d8c493d9b4e346861583428c6810944cffbc2f7
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-04-19 20:15:37 -0400

    add more function to review

--- MInHeapA3/BST.py
+++ MInHeapA3/BST.py
@@ -65,15 +65,16 @@ class BST:
             self.count += 1
         self.count_helper(node.r, x)
 
-    def max_value(self):
-        def max_value_helper(node):
+    def max_value_preorder(self):
+        # Helper function for pre-order traversal to find the maximum value
+        def preorder_helper(node):
             if node is None:
-                return
-            left_node = max_value_helper(node.l)
-            right_node = max_value_helper(node.r)
-            return max(left_node, right_node)
+                return float('-inf')  # Return negative infinity for comparison
+            max_left = preorder_helper(node.l)  # Recursively traverse left subtree
+            max_right = preorder_helper(node.r)  # Recursively traverse right subtree
+            return max(node.data, max_left, max_right)  # Return maximum value among current node and its subtrees
 
-        return max_value_helper(self.root)
+        return preorder_helper(self.root)
 
     # In this implementation, the find_second_largest method uses a recursive helper function _in_order_traversal to
     # perform an in-order traversal of the binary search tree in reverse order (right, root, left) and keep track of
@@ -101,6 +102,42 @@ class BST:
         if count[0] < 2:
             return None
         return second_largest
+
+    def is_value_present(self, value):
+        return self.helper_is_value(self.root, value)
+
+    def helper_is_value(self, node, value):
+        if node is None:
+            return False
+        if node.data == value:
+            return True
+        if value < node.data:
+            return self.helper_is_value(node.l, value)
+        else:
+            return self.helper_is_value(node.r, value)
+
+    def is_value_present_inorder(self, value):
+        return self.helper_inorder_value(self.root, value)
+
+    def sum_all_node(self):
+        return self.sum_helper(self.root, 0)
+
+    def sum_helper(self, node, sum5):
+        if node is None:
+            return 0
+        lef = self.sum_helper(node.l, sum5)
+        rig = self.sum_helper(node.r, sum5)
+        return node.data + lef + rig
+
+    def helper_inorder_value(self, node, value):
+        if not node:
+            return False
+        if self.helper_inorder_value(node.l, value):
+            print(node.data)
+            return True
+        if node.data == value:
+            return True
+        return self.helper_inorder_value(node.r, value)
 
     def ins(self, data):
         if self.root is None:
@@ -196,7 +233,18 @@ class BST:
             if node.r:
                 stack.append(node.r)
         print(count)
-        return count
+        return counะ
+
+    def sum_max_value(self):
+        def helper(node):
+            if node is None:
+                return
+            max_value = max(lt, rt)
+            left = helper(node.l)
+            right = helper(node.r)
+            return max_value
+
+        return helper(self.root)
 
     def find_sum(self, node, sum):
         if node is None:
@@ -205,7 +253,7 @@ class BST:
             return node.data == sum
         left_sum = self.find_sum(node.l, sum - node.data)
         right_sum = self.find_sum(node.r, sum - node.data)
-        return left_sum and right_sum
+        return left_sum + right_sum
 
     def find_sum2(self, node):
         if node is None:
@@ -222,6 +270,33 @@ class BST:
             return self.max_value(n.r)
 
     # Write a Python method in class BT that returns the sum of all node values in the tree using post-order traversal.
+    def height(self):
+        if self.root is None:
+            return 0
+        height = 0
+        stack = [self.root]
+        while stack:
+            level = len(stack)
+            for i in range(level):
+                node = stack.pop(0)
+                if node.l:
+                    stack.append(node.l)
+                if node.r:
+                    stack.append(node.r)
+            height += 1
+        return height
+
+    def height_recur(self):
+        def height_helper(node):
+            if node is None:
+                return 0
+            else:
+                hl = height_helper(node.l)
+                hr = height_helper(node.r)
+                return max(hl, hr) + 1
+
+        return height_helper(self.root)
+
     def post_sums(self, node):
         if node is None:
             return 0
@@ -243,9 +318,11 @@ class BST:
             return 0
         c_sum = 0
         if node.data % 2 == 0:
-            print(node.data)
+            print("Even Node found: ", node.data)
             c_sum = node.data
+        print("traversing left subtree of node with data:", node.data)
         left_sum = self.sum_inOrder(node.l)
+        print("traversing right subtree of node with data:", node.data)
         r_sum = self.sum_inOrder(node.r)
         return c_sum + left_sum + r_sum
 
@@ -271,6 +348,35 @@ class BST:
     # Write an iterative method in the BT class called print_leaf_nodes that prints all the leaf nodes of the binary
     # tree using an in-order traversal. A leaf node is defined as a node without any children. Make sure to include
     # the inner class Node and the constructor in the BT class.
+    def iterative_in_order(self):
+        stack = []
+        node = self.root
+        while True:
+            while node is not None:
+                stack.append(node)
+                node = node.l
+            if len(stack) == 0:
+                return
+            node = stack.pop()
+            print(node.data, end=" > ")
+            node = node.r
+
+    def print_between(self, min, max):
+        print(min, max)
+        print("=======")
+
+        def print_between_helper(node, min, max):
+            if node is None:
+                return
+            if min < node.data:
+                print_between_helper(node.l, min, max)
+            if min < node.data < max:
+                print(node.data)
+            if max > node.data:
+                print_between_helper(node.r, min, max)
+
+        print_between_helper(self.root, min, max)
+
     def print_leaf_node(self):
         if self.root is None:
             return None
@@ -304,6 +410,31 @@ class BST:
         search_helper(self.root, target)
         return count[0]
 
+        def inorderTravelsal(self, root):
+            res = []
+            stack = []
+            curr = root
+            while curr or stack:
+                while cure:
+                    stack.append(curl)
+                    curr = curr.left
+                curr = stack.pop()
+                res.append(cur.val)
+                cur = curr.right
+            return res
+
+        def incurInOrder(self, rott):
+            def inRecur(self, root):
+                if not root:
+                    return True
+                else:
+                    inRecur(root.left)
+                res.append(root.val)
+                inRecur(root.right)
+
+            inOrder(root)
+            return res
+
 
 """
 Node => Null
@@ -325,7 +456,6 @@ if __name__ == "__main__":
     print("MAXMIMUM value <", root.max_value(root.root), ">")
     print("SUM VALUE <", root.post_sums(root.root), ">")
     print("COUNT NODE <", root.count_node(root.root), ">")
-    print("Even Node Sum <", root.sum_inOrder(root.root), ">")
     print("Min dif BST <", root.find_min_diff(8))
     root.print_leaf_node()
     print("\nCounter less than target", root.count_less_than_target(15))
@@ -338,4 +468,16 @@ if __name__ == "__main__":
     print("\n Find second largest value <", root.find_second_largest(), ">")
     root.bfs()
     print("\n Search Less value than target 12 <", root.search_less_target(root.root, 12), ">")
-    print("============================")
+    print("Even Node Sum <", root.sum_inOrder(root.root), ">")
+    print("Iterative In-order <", root.iterative_in_order())
+    print("\n Find sum 1 and 2, ", root.find_sum2(root.root))
+    print("============== CHECK VALUE ================")
+    root.bfs()
+    print('\n Check value is presented  yet? <', root.is_value_present(7))
+    print('\n Check value is presented  yet? <', root.is_value_present_inorder(1))
+    print("\n Sum total node value <", root.sum_all_node(), ">")
+    print("\n Print between the give value >", root.print_between(7, 15), ">")
+    print("\n Height of My tree <", root.height(), ">")
+    print("\n Height of My tree <", root.height_recur(), ">")
+    print("\n Sum Max Value of all the node<", root.max_value_preorder(), ">")
+    print("===========================================")
